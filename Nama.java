9c5c2fa2ff11b2a53cfce151532bdbe8af5eaf68
RizkiEggi <48307776+RizkiEggi@users.noreply.github.com>
2019-03-07 20:27:23 +0700

    Add files via upload

---
+++ Nama.java
@@ -0,0 +1,10 @@
+package newpackage;
+
+public class Nama {
+    
+    public static void main(String[] args) {
+        System.out.println("NAMA  = Rizki Eggi Pranoto");
+        System.out.println("NIM   = 17090083");
+        System.out.println("KELAS = 4A ");
+    }
+}
